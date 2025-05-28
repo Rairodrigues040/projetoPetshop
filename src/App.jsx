0dadf676e3c2b8c8ec58ab4ebd375c8d51025b71
gustavosantosl <gustavosantos2166@gmail.com>
2025-05-28 14:22:46 -0300

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+import Header from './components/header/Header';
+import './App.css';
 
 const App = () => {
   return (
-    <div>
-      <p>Olá, Time includers</p>
+    <div className="page">
+      <Header />
+      <main>
+        <p>Olá, Time Includers</p>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
