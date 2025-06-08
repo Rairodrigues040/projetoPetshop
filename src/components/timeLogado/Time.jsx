baacf5ce735cea5cb7677bb3475ad977b6490709
gustavosantosl <gustavosantos2166@gmail.com>
2025-06-08 20:43:32 -0300

    up

--- src/components/timeLogado/Time.jsx
+++ src/components/timeLogado/Time.jsx
@@ -1,14 +1,20 @@
-import '../time/Time.css';
+import './Time.css';
 import React from 'react';
+import Marina from "./marina.jpg";
+import Rai from "./rai.jpg";
+import Alice from "./alice.jpg";
+import Gustavo from "./gustavo.jpg";
+
+
 
 const equipe = [
-  { iniciais: 'MR', nome: 'Marina Lima', cargo: 'Gerente de projetos', cor: 'azul', descricao: 'Adotar mudou minha vida com muito amor, carinho e patinhas pela casa.' },
-  { iniciais: 'RR', nome: 'Raí Rodrigues', cargo: 'Dev Front-End', cor: 'laranja', descricao: 'Cada linha de código aqui é por eles e por um final feliz. Adote!' },
-  { iniciais: 'AL', nome: 'Alice Lima', cargo: 'UI/UX Designer', cor: 'roxo', descricao: 'Tenho dois cachorros adotados e sei bem o quanto essa causa importa!' },
-  { iniciais: 'KR', nome: 'Kaio Sousa', cargo: 'Dev Back-End', cor: 'verde', descricao: 'Veterinária especializada em pequenos animais, dedica sua vida ao cuidado e bem estar animal!' },
-  { iniciais: 'GS', nome: 'Gustavo Santos', cargo: 'Dev Front-End', cor: 'laranja-escuro', descricao: 'A energia e a alegria que meus pets trazem pro dia a dia não têm preço. Apoiar essa causa é apoiar o amor em sua forma mais pura.' },
-  { iniciais: 'AL', nome: 'Alisson Paz', cargo: 'UI/UX Designer', cor: 'roxo', descricao: 'Veterinária especializada em pequenos animais, dedica sua vida ao cuidado e bem estar animal!' },
+  { foto: Marina, nome: 'Marina Lima', cargo: 'Gerente de projetos', descricao: 'Adotar mudou minha vida com muito amor, carinho e patinhas pela casa.' },
+  { foto: Rai, nome: 'Raí Rodrigues', cargo: 'Dev Front-End', descricao: 'Cada linha de código aqui é por eles e por um final feliz. Adote!' },
+  { foto: Alice, nome: 'Alice Lima', cargo: 'UI/UX Designer', descricao: 'Tenho dois cachorros adotados e sei bem o quanto essa causa importa!' },
+  { foto: Gustavo, nome: 'Gustavo Santos', cargo: 'Dev Front-End', descricao: 'A energia e a alegria que meus pets trazem pro dia a dia não têm preço. Apoiar essa causa é apoiar o amor em sua forma mais pura.' },
+  
 ];
+
 
 function Time() {
   return (
@@ -17,7 +23,8 @@ function Time() {
       <div className="grid-equipe">
         {equipe.map((membro, index) => (
           <div key={index} className="card-membro">
-            <div className={`avatar ${membro.cor}`}>{membro.iniciais}</div>
+            <img src={membro.foto} alt={membro.nome} className="avatar-foto" />
+
             <h3>{membro.nome}</h3>
             <p className="cargo">{membro.cargo}</p>
             <p className="descricao">{membro.descricao}</p>
